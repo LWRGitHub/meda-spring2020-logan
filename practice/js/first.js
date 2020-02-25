b72fac65547df28bb813f754574b32da847057c2
LWRGitHub <loganreynoldslwr@gmail.com>
2020-02-25 09:19:21 -0800

    javascript lessone 1 console.log & data types

---
+++ practice/js/first.js
@@ -0,0 +1,42 @@
+console.log(10 *10);
+
+console.log(1000);
+
+console.log(1337);
+
+//Strings are always
+console.log("eddie");
+
+console.log('Hello, "how are you?');
+
+console.log("100");
+
+console.log("100");
+
+console.log("hello" * 10);
+
+//You cannot do math on strings.
+console.log();
+
+
+
+
+//The Boolean datatype
+console.log(true);
+console.log()
+
+console.log(1);
+console.log(0);
+
+console.log();
+
+1000.000;
+.0001;
+"1000";
+"hundred";
+true;
+NaN;
+
+
+
+console.log(100/0);
