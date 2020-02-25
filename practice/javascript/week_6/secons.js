528ca6efaf01ad28fb5e73e9b28c9cf96f70e3cf
LWRGitHub <loganreynoldslwr@gmail.com>
2020-02-25 10:51:37 -0800

    js files second & third, second is compleat third in progress

---
+++ practice/javascript/week_6/secons.js
@@ -0,0 +1,56 @@
+// Variables
+
+console.log("Start of Second script.")
+
+console.log(10 + 10);
+console.log(10 +21);
+
+var x;
+
+x = "Hello";
+
+console.log(x);
+
+
+var usersAge;
+var usersAdress;
+
+// don't rite like this.
+var thisclassissomewhatokay;
+
+// Camel Case, most popular.
+var thisClassIsSomewhatOkay;
+
+//Snake Case, second most popular, especially Python.
+var this_class_is_somewhat_okay;
+
+thisClassIsSomewhatOkay = 100;
+console.log(thisClassIsSomewhatOkay);
+
+// Single Line Comment
+
+/* Multi
+Line
+Comment
+*/
+
+
+
+var myVar = true;
+
+console.log(myVar);
+
+myVar;
+
+var coolVar = 1000;
+
+console.log("CoolVar is holding " + coolVar + " on line 47.");
+
+coolVar = 2000;
+
+console.log(coolVar);
+
+var added = "hello " + (1000 + 50);
+
+console.log(added);
+
---
+++ practice/javascript/week_6/third.js
@@ -0,0 +1,4 @@
+var myFirstVar = 1000;
+
+var yearsLeft = 1000;
+var buildingHeight = 100;
