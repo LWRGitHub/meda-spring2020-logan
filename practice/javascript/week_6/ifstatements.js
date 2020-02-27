45f84b691d9059f62589c72ba9cc5febc832e066
LWRGitHub <loganreynoldslwr@gmail.com>
2020-02-27 14:51:52 -0800

    if statements

---
+++ practice/javascript/week_6/ifstatements.js
@@ -0,0 +1,29 @@
+// If-Else Statements, AKA conditional statements
+// if (conditiona) {}
+
+if(true){
+    console.log("This code ran if the if statement was true.")
+}
+
+console.log("End of script");
+
+//Comparison operators or "Comparitors"
+/*
+== equal to 
+>= greater thatn or equal
+> greater than
+< lesser than
+!= not equal (should not equal to be true)
+*/
+
+var age = 22;
+
+if(age >= 21){
+    console.log("Congratz!");
+}
+
+var password = 123456;
+
+if (password == "abcdef"){
+    console.log("welcome to your profile!");
+}
