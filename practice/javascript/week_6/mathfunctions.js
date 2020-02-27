87e3eed5bba6cfa3d32110b249ef0855ec98b069
LWRGitHub <loganreynoldslwr@gmail.com>
2020-02-27 11:45:35 -0800

    js math functions

--- practice/javascript/week_6/mathfunctions.js
+++ practice/javascript/week_6/mathfunctions.js
@@ -25,3 +25,74 @@ var laptopPrice = 999.95;
 var laptopTax = taxCalculator(laptopPrice);
 
 console.log(`$${laptopTax + laptopPrice}`);
+
+/*
+
+Create a function called mathifier
+
+This function will take two Numbers
+
+with these two numbers, you will add them, then add the sum to the first number, and then multiply that by the second number.
+
+The function will return the result.
+
+*/
+
+function mathifier(num1, num2){
+    return ((num1 + num2) + num1) * num2;
+}
+
+console.log(mathifier(2,4));
+
+/*
+
+This function will be called mathResaults
+
+This fuction take only 2 numbers.
+
+This function will console.log Each of the following:
+The sum of these two numbers.
+The difference of these two numbers.
+the product of these two numbers.
+the dividend of these two numbers.
+the leftovers of these two numbers. (modulo)
+
+*/
+
+function mathResaults(num1, num2) {
+    console.log(num1, num2);
+    console.log(num1 + num2);
+    if(num1 <= num2){
+        console.log(num2 - num1);
+    } else {
+        console.log(num1 - num2);
+    }
+    console.log(num1 * num2);
+    if(num1 <= num2){
+        console.log(num2 / num1);
+    } else {
+        console.log(num1 / num2);
+    }
+    console.log(num1 % num2);
+    console.log(num2 % num1);
+}
+
+mathResaults(2,5);
+
+function mathResaults2(num1, num2){
+
+    console.log(num1, num2);
+    console.log(num1 + num2);
+
+    console.log(num1 - num2);
+
+    console.log(num1 * num2);
+
+    console.log(num1 / num2);
+
+    console.log(num1 % num2);
+
+}
+
+mathResaults2(2,5);
+
