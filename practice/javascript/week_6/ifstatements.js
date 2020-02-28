6b3660250ee447a9dd3e45b7bf10b38f25bc5af9
LWRGitHub <loganreynoldslwr@gmail.com>
2020-02-28 11:08:10 -0800

    if statments && || ! true false truthy falsy

--- practice/javascript/week_6/ifstatements.js
+++ practice/javascript/week_6/ifstatements.js
@@ -27,3 +27,66 @@ var password = 123456;
 if (password == "abcdef"){
     console.log("welcome to your profile!");
 }
+
+
+var test = "1000";
+
+if(test == 1000){
+    console.log("Test!")
+}
+
+var sayHello;
+
+//If and Else parts
+if(sayHello){
+    console.log("hello");
+} else {
+    console.log("goodbye");
+}
+
+//Truthy and Falsey values.
+
+//If a variable is holding any of the following it will become a false boolean:
+/*
+LIST OF MOST COMMON FALSYS
+undefined
+null 
+0
+""
+NAN
+
+LIST OF MOST COMMON TRUTHYS
+ANYTHING ELSE
+"0"
+-1
+
+*/
+
+var checkIfTrue = "0";
+
+if (checkIfTrue){
+    console.log("The " + checkIfTrue + " value is truthy");
+} else {
+    console.log("The '" + checkIfTrue + "' value is falsy.");
+}
+
+//Logical Onerators: && || !
+/*
+&& = both sides must be true.
+|| = wither side must be true.
+! = flip the current boolean. (if true, it becomes false, and if false, it becomes true).
+*/
+
+var firstBool = true;
+var secondBool = false;
+
+if(firstBool || secondBool){
+    console.log("This has runned because the condition is true.")
+}
+
+var level;
+
+if (level){
+    console.log("You can")
+}
+
