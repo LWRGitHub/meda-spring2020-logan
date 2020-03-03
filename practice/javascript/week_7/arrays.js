d009d8c14ac90e0425494151b456f9dc845c3089
LWRGitHub <loganreynoldslwr@gmail.com>
2020-03-03 09:13:16 -0800

    update

---
+++ practice/javascript/week_7/arrays.js
@@ -0,0 +1,29 @@
+var myVar = "One Item";
+var firstDay = "monday";
+var secondDay = "tuesday";
+
+// Array Syntax, a collection of data that is surrounded by square brackets.
+
+var daysOfWeek = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturnoday", "sunday"];
+
+// Requesting a spacific value inside of the array:
+// Use square brackets after calling the variable with the array.
+// Put the index number snside of the square brackets.
+// Example myArray[7];
+
+
+console.log(daysOfWeek[6]);
+
+daysOfWeek[6] = "funday";
+
+console.log(daysOfWeek[6]);
+
+daysOfWeek[12] = "superday";
+
+console.log(daysOfWeek);
+
+// You can mix datatypes in an Array.
+//[100, true, "hello"];
+
+
+
