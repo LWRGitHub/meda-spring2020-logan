0346b3ab952b04de77f32cece750acceab57566c
LWRGitHub <loganreynoldslwr@gmail.com>
2020-03-04 11:22:28 -0800

    objexts deep explenation & stringmethods with array methods

---
+++ practice/javascript/week_7/stringmethods.js
@@ -0,0 +1,26 @@
+//Manually replace the question mark.
+
+var myString = "Hello there how are you?";
+
+console.log(myString.length);
+
+var myStringArray = myString.split('');
+
+console.log(myStringArray);
+
+myStringArray[23] = ".";
+
+console.log(myStringArray);
+
+myStringArray.pop();
+myStringArray.pop();
+myStringArray.shift();
+myStringArray.shift();
+
+var finishedString = myStringArray.join("");
+
+console.log(finishedString);
+
+
+
+
