c7151efdc9e9c406bc9130c0cbd5de74032dde2f
LWRGitHub <loganreynoldslwr@gmail.com>
2020-03-05 11:00:09 -0800

    paramid builder coments and update code

--- practice/javascript/week_7/pyramidbuilder_arguments.js
+++ practice/javascript/week_7/pyramidbuilder_arguments.js
@@ -27,20 +27,21 @@ function pyramidBuilder(height) {
 
 }
 
-
-
-
-
+//Gets an array built by rocess.argv and stores in arguments variable.
 var arguments = process.argv;
 console.log(arguments);
 
+// We grab the 3rd item from the arguments array and store it in height.
 var height = arguments[2];
+
+// We grab the 4th from arguments array and convert it to a number and then store in loops.
 var loops = parseInt( arguments[3] );
 
+// Take value of height, convert into a number, put it back into height.
 height = parseInt(height);
 console.log(height);
 
-
+// We loop fro the amount of loops, and run pyramidBuilder with the approriate height value.
 for (var i = 0; i < loops; i++){    
     pyramidBuilder(height);
 }
