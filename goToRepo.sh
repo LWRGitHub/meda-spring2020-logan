db651f0465b1e75c933bdb2578f303e7ee8ba627
LWRGitHub <loganreynoldslwr@gmail.com>
2020-03-05 12:14:09 -0800

    update

---
+++ goToRepo.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /Documents/meda-spring2020-eduardo/javascript/week_7
+
--- practice/javascript/week_7/preader.js
+++ practice/javascript/week_7/preader.js
@@ -1,3 +1,24 @@
 var arguments = process.argv;
 
 console.log("Contents of process.argv", arguments);
+
+
+
+
+
+
+
+
+function wordCount(testText){
+    var textArray = testText.split(" ");
+
+    //console.log(textArray);
+    console.log("The text: " + testText + " has a total of " +textArray.length + " words in it.");
+}
+
+
+wordCount("hello how are you doing? I am fine, the weather is nice!");
+
+
+
+
