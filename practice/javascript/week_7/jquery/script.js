af24f74ca1ec02453408afbc2a6ec9fcfb229ebf
LWRGitHub <loganreynoldslwr@gmail.com>
2020-03-09 11:14:04 -0700

    jQuery .remove .css .append .prepend .click .empty

--- practice/javascript/week_7/jquery/script.js
+++ practice/javascript/week_7/jquery/script.js
@@ -3,29 +3,45 @@
 
 jQuery(document).ready( function () {
 
-    console.log("hello");
+    $("html").remove();
 
-    jQuery("h1").css(
-        {
-            "font-size": "100px",
-            "color": "pink"
-        }
-    );
-
-    jQuery("ul").css({
-        "border": "10px solid green"
+    $("#light-off").click(function () {
+        $("body").css({"background-color": "black"});
+        $("body").css("color", "white");
     });
 
-    jQuery("button").click( function() {
-        console.log("hello")
+    $("#light-on").click(function () {
+        var body = $("body");
 
-        jQuery("body").css({
-            "background-color": "black"
-        });
+        body.css({"background-color": "white"});
+        body.css("color", "black");
+});
 
-        jQuery("h1").css({
-            "margin-top": "500px"
-        });
+    // new JavaScript function, takes two arguments, first is callback, second is how long in milliseconds.
+    setTimeout(function () {
+
+        // Add an element to the webpage. .append()
+        $("ol").append("<li>This was added ager the fact.</li>");
+
+        $("body").append("<h1>New section<h1><p>Welcome to this new section</p>");
+
+        // Adds an element to the start of the child list, .prepend()
+        $("body").prepend("<h1 style='color: red'>Alert! new things!</h1>");
+
+        //Adds an html as a child, replacing existing children, .html()
+
+        $("ol").html("<li>All tasks done!</li>")
+
+    }, 5000);
+
+    $("#recycleButton").click(function (){
+
+        // Removes the element you have selected.
+        $('body').remove();
+
+        // Removes all children of the element you have selected.
+        $("body").empty();
+
     });
 
 });
